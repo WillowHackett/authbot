4ccc2c8b4c78503d04c676776cd621fe08360060
Lylia <71930916+dishapatel010@users.noreply.github.com>
2022-03-22 14:48:36 +0530

    Update and rename configFile.js to config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports.domain = process.env.DOMAIN,
+module.exports.demo = process.env.DEMO,
+module.exports.bot = process.env.BOTUSERNAME,
+module.exports.secret = process.env.SECRET,
+module.exports.token = process.env.BOT_TOKEN,
+module.exports.admin = process.env.ADMIN_USERNAME,
+module.exports.hubRedis = process.env.REDISURL
